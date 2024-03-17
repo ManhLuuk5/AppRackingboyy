570daeb1ef61be3624fb8ff3d456ef3859a14f9e
luumanh <luumanhduc3105@gmail.com>
2024-03-17 21:49:09 +0700

    data app Racking

--- app/src/main/java/com/example/rackingboybooooo/MainActivity.kt
+++ app/src/main/java/com/example/rackingboybooooo/MainActivity.kt
@@ -1,150 +1,107 @@
 package com.example.rackingboybooooo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
-import android.widget.CheckBox
-import android.widget.ImageView
-import android.widget.SeekBar
 import android.widget.Toast
-import androidx.core.view.isVisible
-import kotlin.random.Random
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.rackingboybooooo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var p1: ImageView
-    private lateinit var s1: SeekBar
-    private lateinit var s2: SeekBar
-    private lateinit var s3: SeekBar
-    private lateinit var b1: CheckBox
-    private lateinit var b2: CheckBox
-    private lateinit var b3: CheckBox
-
-
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var timer: CountDownTimer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        with(binding) {
+            val a = 50000L
+            val b = 150L
+             timer = object : CountDownTimer(a, b) {
+                override fun onTick(millisUntilFinished: Long) {
 
-        play()
-        enableCheckbox()
-        s1.isEnabled = true
-        s2.isEnabled = true
-        s3.isEnabled = true
+                    val rd = (0..5).random()
+                    val rd2 = (0..5).random()
+                    val rd3 = (0..5).random()
 
+                    seekbar1.progress += rd
+                    seekbar2.progress += rd2
+                    seekbar3.progress += rd3
+                    play1.visibility = View.VISIBLE
+                    if (seekbar1.progress >= seekbar1.max) {
+                        showToastAndCancel("Winner 1")
+                    } else if (seekbar2.progress >= seekbar2.max) {
+                        showToastAndCancel("Winner 2")
+                    } else if (seekbar3.progress >= seekbar3.max) {
+                        showToastAndCancel("Winner 3")
+                    }
+                    if(checkbox1.isEnabled ){
+                        setZeroProgress()
+                    }else play1.visibility = View.INVISIBLE
+                }
 
-        val timer = object : CountDownTimer(60000, 300) {
-            override fun onTick(millisUntilFinished: Long) {
-                val rd = (0..5).random()
-                val rd2 = (0..5).random()
-                val rd3 = (0..5).random()
-
-                s1.progress += rd
-                s2.progress += rd2
-                s3.progress += rd3
-
-                if (s1.progress >= s1.max) {
-                    this.cancel()
-                    p1.visibility = View.VISIBLE
-                    Toast.makeText(this@MainActivity, "Winner 1", Toast.LENGTH_SHORT).show()
-                    enableCheckbox()
-
-                } else if (s2.progress >= s2.max) {
-                    this.cancel()
-                    p1.visibility = View.VISIBLE
-                    Toast.makeText(this@MainActivity, "Winner 2", Toast.LENGTH_SHORT).show()
-                    enableCheckbox()
-
-                } else if (s3.progress >= s3.max) {
-                    this.cancel()
-                    p1.visibility = View.VISIBLE
-                    Toast.makeText(this@MainActivity, "Winner 3", Toast.LENGTH_SHORT).show()
-                    enableCheckbox()
+                override fun onFinish() {
 
                 }
+            }
+
+            play1.setOnClickListener {
+                timer.start()
+                seekbar1.isEnabled = false
+                seekbar2.isEnabled = false
+                seekbar3.isEnabled = false
+                enableCheckbox(false)
 
             }
 
-            override fun onFinish() {
-
+            checkbox1.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    checkbox2.isChecked = false
+                    checkbox3.isChecked = false
+                    play1.visibility = View.VISIBLE
+                } else {
+                    play1.visibility = View.INVISIBLE
+                }
+            }
+            checkbox2.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    checkbox3.isChecked = false
+                    checkbox1.isChecked = false
+                    play1.visibility = View.VISIBLE
+                } else {
+                    play1.visibility = View.INVISIBLE
+                }
+            }
+            checkbox3.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    checkbox1.isChecked = false
+                    checkbox2.isChecked = false
+                    play1.visibility = View.VISIBLE
+                } else {
+                    play1.visibility = View.INVISIBLE
+                }
             }
         }
+    }
+    private fun showToastAndCancel(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        timer.cancel()
+        enableCheckbox(true)
+    }
 
-
-        b1.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                b2.isChecked = false
-                b3.isChecked = false
-                p1.visibility = View.VISIBLE
-                p1.setOnClickListener {
-                    s1.progress = 0
-                    s2.progress = 0
-                    s3.progress = 0
-                    p1.visibility = View.INVISIBLE
-                    timer.start()
-                    UnenableCheckbox()
-                }
-            } else p1.visibility = View.INVISIBLE
-
+    private fun enableCheckbox(isEnable: Boolean) {
+        with(binding) {
+            checkbox1.isEnabled = isEnable
+            checkbox2.isEnabled = isEnable
+            checkbox3.isEnabled = isEnable
         }
+    }
 
-        b2.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                b3.isChecked = false
-                b1.isChecked = false
-                p1.visibility = View.VISIBLE
-                p1.setOnClickListener {
-                    s1.progress = 0
-                    s2.progress = 0
-                    s3.progress = 0
-                    p1.visibility = View.INVISIBLE
-                    timer.start()
-                    UnenableCheckbox()
-                }
-            } else p1.visibility = View.INVISIBLE
-
+    private fun setZeroProgress() {
+        with(binding) {
+            seekbar1.progress = 0
+            seekbar2.progress = 0
+            seekbar3.progress = 0
         }
-
-        b3.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                b1.isChecked = false
-                b2.isChecked = false
-                p1.visibility = View.VISIBLE
-                p1.setOnClickListener {
-                    s1.progress = 0
-                    s2.progress = 0
-                    s3.progress = 0
-                    p1.visibility = View.INVISIBLE
-                    timer.start()
-                    UnenableCheckbox()
-                }
-            } else p1.visibility = View.INVISIBLE
-        }
-
     }
-
-
-
-    private fun enableCheckbox() {
-        b1.isEnabled = true
-        b2.isEnabled = true
-        b3.isEnabled = true
-    }
-
-    private fun UnenableCheckbox() {
-        b1.isEnabled = false
-        b2.isEnabled = false
-        b3.isEnabled = false
-    }
-
-    private fun play() {
-        s1 = findViewById(R.id.s1)
-        s2 = findViewById(R.id.s2)
-        s3 = findViewById(R.id.s3)
-        p1 = findViewById(R.id.p1)
-        b1 = findViewById(R.id.b1)
-        b2 = findViewById(R.id.b2)
-        b3 = findViewById(R.id.b3)
-    }
-
 }
