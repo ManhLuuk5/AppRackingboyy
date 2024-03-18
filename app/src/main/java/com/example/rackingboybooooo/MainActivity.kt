f282ba540abca80479b2250b2a9f041cacd63f65
luumanh <luumanhduc3105@gmail.com>
2024-03-18 21:38:04 +0700

    data binding App Racking

--- app/src/main/java/com/example/rackingboybooooo/MainActivity.kt
+++ app/src/main/java/com/example/rackingboybooooo/MainActivity.kt
@@ -9,80 +9,44 @@ import androidx.databinding.DataBindingUtil
 import com.example.rackingboybooooo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var timer: CountDownTimer
+    private val MAX_TIME_RUNNING = 50000L
+    private val STEP_RUNNING = 150L
+    private var binding: ActivityMainBinding ?= null
+    private var timer: CountDownTimer = object : CountDownTimer(MAX_TIME_RUNNING, STEP_RUNNING) {
+        override fun onTick(millisUntilFinished: Long) {
+            if (binding != null) {
+                var checkNull = binding!!
+                checkNull.seekbar1.progress += (0..5).random()
+                checkNull.seekbar2.progress += (0..5).random()
+                checkNull.seekbar3.progress += (0..5).random()
+                checkNull.imAvatar.visibility = View.VISIBLE
+                if ((binding?.seekbar1?.progress ?: 0) >= (binding?.seekbar1?.max ?: 0)) {
+                    showToastAndCancel("Winner 1")
+                } else if ((binding?.seekbar2?.progress ?: 0) >= (binding?.seekbar2?.max ?: 0)) {
+                    showToastAndCancel("Winner 2")
+                } else if ((binding?.seekbar3?.progress ?: 0) >= (binding?.seekbar3?.max ?: 0)) {
+                    showToastAndCancel("Winner 3")
+                }
+
+                if (binding?.cbPlayer2?.isEnabled == true) {
+                    setZeroProgress()
+                } else binding?.imPlay?.visibility = View.INVISIBLE
+            }
+        }
+
+        override fun onFinish() {
+
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        with(binding) {
-            val a = 50000L
-            val b = 150L
-             timer = object : CountDownTimer(a, b) {
-                override fun onTick(millisUntilFinished: Long) {
-
-                    val rd = (0..5).random()
-                    val rd2 = (0..5).random()
-                    val rd3 = (0..5).random()
-
-                    seekbar1.progress += rd
-                    seekbar2.progress += rd2
-                    seekbar3.progress += rd3
-                    play1.visibility = View.VISIBLE
-                    if (seekbar1.progress >= seekbar1.max) {
-                        showToastAndCancel("Winner 1")
-                    } else if (seekbar2.progress >= seekbar2.max) {
-                        showToastAndCancel("Winner 2")
-                    } else if (seekbar3.progress >= seekbar3.max) {
-                        showToastAndCancel("Winner 3")
-                    }
-                    if(checkbox1.isEnabled ){
-                        setZeroProgress()
-                    }else play1.visibility = View.INVISIBLE
-                }
-
-                override fun onFinish() {
-
-                }
-            }
-
-            play1.setOnClickListener {
-                timer.start()
-                seekbar1.isEnabled = false
-                seekbar2.isEnabled = false
-                seekbar3.isEnabled = false
-                enableCheckbox(false)
-
-            }
-
-            checkbox1.setOnCheckedChangeListener { _, isChecked ->
-                if (isChecked) {
-                    checkbox2.isChecked = false
-                    checkbox3.isChecked = false
-                    play1.visibility = View.VISIBLE
-                } else {
-                    play1.visibility = View.INVISIBLE
-                }
-            }
-            checkbox2.setOnCheckedChangeListener { _, isChecked ->
-                if (isChecked) {
-                    checkbox3.isChecked = false
-                    checkbox1.isChecked = false
-                    play1.visibility = View.VISIBLE
-                } else {
-                    play1.visibility = View.INVISIBLE
-                }
-            }
-            checkbox3.setOnCheckedChangeListener { _, isChecked ->
-                if (isChecked) {
-                    checkbox1.isChecked = false
-                    checkbox2.isChecked = false
-                    play1.visibility = View.VISIBLE
-                } else {
-                    play1.visibility = View.INVISIBLE
-                }
-            }
-        }
+        setOnClick()
+        onCheckedChangeListener()
     }
+
+
     private fun showToastAndCancel(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
         timer.cancel()
@@ -90,18 +54,56 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun enableCheckbox(isEnable: Boolean) {
-        with(binding) {
-            checkbox1.isEnabled = isEnable
-            checkbox2.isEnabled = isEnable
-            checkbox3.isEnabled = isEnable
+        binding?.cbPlayer1?.isEnabled = isEnable
+        binding?.cbPlayer2?.isEnabled = isEnable
+        binding?.cbPlayer3?.isEnabled = isEnable
+    }
+
+    private fun onCheckedChangeListener() {
+        binding?.cbPlayer1?.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding?.cbPlayer2?.isChecked = false
+                binding?.cbPlayer3?.isChecked = false
+                binding?.imPlay?.visibility = View.VISIBLE
+            } else {
+                binding?.imPlay?.visibility = View.INVISIBLE
+            }
+        }
+        binding?.cbPlayer2?.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding?.cbPlayer3?.isChecked = false
+                binding?.cbPlayer1?.isChecked = false
+                binding?.imPlay?.visibility = View.VISIBLE
+            } else {
+                binding?.imPlay?.visibility = View.INVISIBLE
+            }
+        }
+        binding?.cbPlayer3?.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding?.cbPlayer1?.isChecked = false
+                binding?.cbPlayer2?.isChecked = false
+                binding?.imAvatar?.visibility = View.VISIBLE
+            } else {
+                binding?.imPlay?.visibility = View.INVISIBLE
+            }
+        }
+    }
+
+
+    private fun setOnClick() {
+        binding?.imPlay?.setOnClickListener {
+            timer.start()
+            binding?.seekbar1?.isEnabled = false
+            binding?.seekbar2?.isEnabled = false
+            binding?.seekbar3?.isEnabled = false
+            enableCheckbox(false)
         }
     }
 
     private fun setZeroProgress() {
-        with(binding) {
-            seekbar1.progress = 0
-            seekbar2.progress = 0
-            seekbar3.progress = 0
-        }
+        binding?.seekbar1?.progress = 0
+        binding?.seekbar2?.progress = 0
+        binding?.seekbar3?.progress = 0
     }
 }
+
