ab24025c931a5107bd3e5ccbdc2248176a104173
luumanh <luumanhduc3105@gmail.com>
2024-03-18 22:19:27 +0700

    edit binding -> letbinding

--- app/src/main/java/com/example/rackingboybooooo/MainActivity.kt
+++ app/src/main/java/com/example/rackingboybooooo/MainActivity.kt
@@ -11,24 +11,24 @@ import com.example.rackingboybooooo.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private val MAX_TIME_RUNNING = 50000L
     private val STEP_RUNNING = 150L
-    private var binding: ActivityMainBinding ?= null
+    private var binding: ActivityMainBinding? = null
     private var timer: CountDownTimer = object : CountDownTimer(MAX_TIME_RUNNING, STEP_RUNNING) {
         override fun onTick(millisUntilFinished: Long) {
-            if (binding != null) {
-                binding!!.seekbar1.progress += (0..5).random()
-                binding!!.seekbar2.progress += (0..5).random()
-                binding!!.seekbar3.progress += (0..5).random()
-                binding!!.imAvatar.visibility = View.VISIBLE
-                if (binding!!.seekbar1.progress>= binding!!.seekbar1.max) {
+            binding?.let { result ->
+                result.seekbar1.progress += (0..5).random()
+                result.seekbar2.progress += (0..5).random()
+                result.seekbar3.progress += (0..5).random()
+                result.imAvatar.visibility = View.VISIBLE
+                if (result.seekbar1.progress >= result.seekbar1.max) {
                     showToastAndCancel("Winner 1")
-                } else if (binding!!.seekbar2.progress >= binding!!.seekbar2.max) {
+                } else if (result.seekbar2.progress >= result.seekbar2.max) {
                     showToastAndCancel("Winner 2")
-                } else if (binding!!.seekbar3.progress >= binding!!.seekbar3.max) {
+                } else if (result.seekbar3.progress >= result.seekbar3.max) {
                     showToastAndCancel("Winner 3")
                 }
-                if (binding!!.cbPlayer2.isEnabled) {
+                if (result.cbPlayer2.isEnabled) {
                     setZeroProgress()
-                } else binding?.imPlay?.visibility = View.INVISIBLE
+                } else result.imPlay.visibility = View.INVISIBLE
             }
         }
 
@@ -52,56 +52,64 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun enableCheckbox(isEnable: Boolean) {
-        binding?.cbPlayer1?.isEnabled = isEnable
-        binding?.cbPlayer2?.isEnabled = isEnable
-        binding?.cbPlayer3?.isEnabled = isEnable
+        binding?.let { result ->
+            result.cbPlayer1.isEnabled = isEnable
+            result.cbPlayer2.isEnabled = isEnable
+            result.cbPlayer3.isEnabled = isEnable
+        }
     }
 
     private fun onCheckedChangeListener() {
-        binding?.cbPlayer1?.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                binding?.cbPlayer2?.isChecked = false
-                binding?.cbPlayer3?.isChecked = false
-                binding?.imPlay?.visibility = View.VISIBLE
-            } else {
-                binding?.imPlay?.visibility = View.INVISIBLE
+        binding?.let { result ->
+            result.cbPlayer1.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    result.cbPlayer2.isChecked = false
+                    result.cbPlayer3.isChecked = false
+                    result.imPlay.visibility = View.VISIBLE
+                } else {
+                    result.imPlay.visibility = View.INVISIBLE
+                }
             }
-        }
-        binding?.cbPlayer2?.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                binding?.cbPlayer3?.isChecked = false
-                binding?.cbPlayer1?.isChecked = false
-                binding?.imPlay?.visibility = View.VISIBLE
-            } else {
-                binding?.imPlay?.visibility = View.INVISIBLE
+            result.cbPlayer2.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    result.cbPlayer3.isChecked = false
+                    result.cbPlayer1.isChecked = false
+                    result.imPlay.visibility = View.VISIBLE
+                } else {
+                    result.imPlay.visibility = View.INVISIBLE
+                }
             }
-        }
-        binding?.cbPlayer3?.setOnCheckedChangeListener { _, isChecked ->
-            if (isChecked) {
-                binding?.cbPlayer1?.isChecked = false
-                binding?.cbPlayer2?.isChecked = false
-                binding?.imAvatar?.visibility = View.VISIBLE
-            } else {
-                binding?.imPlay?.visibility = View.INVISIBLE
+            result.cbPlayer3.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    result.cbPlayer1.isChecked = false
+                    result.cbPlayer2.isChecked = false
+                    result.imAvatar.visibility = View.VISIBLE
+                } else {
+                    result.imPlay.visibility = View.INVISIBLE
+                }
             }
         }
     }
 
 
-    private fun setOnClick() {
-        binding?.imPlay?.setOnClickListener {
-            timer.start()
-            binding?.seekbar1?.isEnabled = false
-            binding?.seekbar2?.isEnabled = false
-            binding?.seekbar3?.isEnabled = false
-            enableCheckbox(false)
+        private fun setOnClick() {
+            binding?.let { result ->
+                result.imPlay.setOnClickListener {
+                    timer.start()
+                    result.seekbar1.isEnabled = false
+                    result.seekbar2.isEnabled = false
+                    result.seekbar3.isEnabled = false
+                    enableCheckbox(false)
+                }
+            }
+        }
+
+        private fun setZeroProgress() {
+            binding?.let { result ->
+                result.seekbar1.progress = 0
+                result.seekbar2.progress = 0
+                result.seekbar3.progress = 0
+            }
         }
     }
-
-    private fun setZeroProgress() {
-        binding?.seekbar1?.progress = 0
-        binding?.seekbar2?.progress = 0
-        binding?.seekbar3?.progress = 0
-    }
-}
 
